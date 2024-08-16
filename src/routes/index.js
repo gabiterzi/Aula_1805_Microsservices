db0b3ac7b7f5c6fbdd2a7b6b04aae83d08d5f351
gabiterzi <gabi.terzig@gmail.com>
2024-08-15 22:16:28 -0300

    status code 403 e 400

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,4 +21,52 @@ router.get('/privada', (req, res) => {
     res.send('Area acessada com sucesso!').status(200)
 })
 
+//403
+const tokenExemplos = {
+    '3' : {role: 'admin'},
+    'tokenUser' : {role: 'user'},
+    'tokenConvidado' : {role: 'convidado'}
+}
+
+router.get('/admin', (req, res) => {
+    // Obtendo o token do header Authorization
+    const token = req.headers['authorization']
+
+    // Verificando se o token foi enviado
+    if (!token) {
+        return res.status(401).send('Unauthorized: token não fornecido')
+    }
+
+    const user = tokenExemplos[token]
+    if (!user) {
+        return res.status(401).send('Unauthorized: token inválido')
+    }
+
+    // Verificando se o usuário tem acesso ao recurso administrativo
+    if (user.role != 'admin') {
+        return res.status(403).send('Forbidden: você não tem permissão para acessar esta área')
+    }
+
+    res.send('Welcome to the admin area!')
+})
+
+// Exemplo bad request 400
+router.post('/submit', (req, res) => {
+    const { username, email } = req.body
+
+    // Verificando se os campos obrigatórios estão presentes
+    if (!username || !email) {
+        return res.status(400).send('Favor preencher todos os campos: username e email.')
+    }
+    // Simulando verificação de email válido
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(email)) {
+        return res.status(400).send('Bad Request: Invalid email format')
+    }
+    // status 202 created
+    // Se tudo estiver certo, continuar com o processamento
+    res.send('Data submitted succesfully!').status(202)
+})
+
+
 module.exports = router;
